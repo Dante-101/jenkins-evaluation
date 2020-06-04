6fb85d711405df93e7a2139f3cb433b2d68a4697
Gaurav Lahoti <gaurav.lahoti@gmail.com>
2020-06-05 04:06:15 +0530

    jenkins file + test

--- sample-app/index.test.ts
+++ sample-app/index.test.ts
@@ -1,5 +1,33 @@
 describe("Sample Test Suite", () => {
-    test("Test", () => {
-        
+    beforeAll(() => { })
+
+    beforeEach(() => { })
+
+    afterEach(() => { })
+
+    afterAll(() => { })
+
+    test("Valid Tests", () => {
+        expect(2 + 2).toBe(4)
+        expect(2 + 1).toEqual(3)
+
+        expect({ one: 1, two: 2 }).toEqual({ one: 1, two: 2 })    //deep check
+        expect({ one: 1, two: 2 }).not.toBe({ one: 1, two: 2 })
+
+        expect(2 + 2).not.toBe(10)
+
+        expect('Christopher').toMatch(/stop/)
+        expect('Christopher').not.toMatch(/1234/)
+
+        expect([1, 2, 4, 5]).toContain(4)
+    })
+
+    test('Exceptions', () => {
+        expect.assertions(1)
+        try {
+            throw 'error'
+        } catch (e) {
+            expect(e).toMatch('error')
+        }
     })
 })
